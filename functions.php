961b57e2e8bffed63a3edb6cdb178a45d6578a3d
keikreutler <kei@ourmachine.net>
2016-07-11 18:34:05 +0200

    Initial style variables in less compiler and added GT Walsheim font files

--- functions.php
+++ functions.php
@@ -5,9 +5,14 @@ function theme_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style )
+        get_stylesheet_directory_uri() . '/style.css'
     );
+
+    wp_enqueue_style('theme-extra',
+    	get_stylesheet_directory_uri().'/styles/custom.less',
+    	array( $parent_style )
+    );
+
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
