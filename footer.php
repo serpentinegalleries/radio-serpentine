3e04918da92cddd64251079d888c199b808b76a2
keikreutler <kei@ourmachine.net>
2016-08-04 16:05:35 +0200

    Fixes issue #3 - enqueue app.js in functions.php to load after Backbone

--- footer.php
+++ footer.php
@@ -9,8 +9,6 @@
 	</div><!-- .site-inner -->
 </div><!-- .site-container -->
 <?php wp_footer(); ?>
-<script src="<?php echo get_site_url(); ?>/wp-content/themes/radio-serpentine/scripts/app.js"></script>
 
-<script src="https://connect.soundcloud.com/sdk/sdk-3.1.2.js"></script>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.css' );
     wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.js' );
     wp_enqueue_script( 'backbone' );
+    wp_enqueue_script( 'app-js', get_stylesheet_directory_uri().'/scripts/app.js' );
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -23,6 +24,7 @@ function theme_enqueue_styles() {
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 'theme_enqueue_scripts' );
 
+/* Removes jQuery Migrate dependency console log */
 add_action( 'wp_default_scripts', function( $scripts ) {
     if ( ! empty( $scripts->registered['jquery'] ) ) {
         $jquery_dependencies = $scripts->registered['jquery']->deps;
