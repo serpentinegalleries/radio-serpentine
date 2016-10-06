0bf3bee6695219f153d7c1f1041752330b101998
keikreutler <kei@ourmachine.net>
2016-10-06 19:09:22 +0100

    PHP function to retrieve live audio metadata

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -224,7 +224,7 @@ radioApp.controller('PlayerInstanceCtrl', function ($uibModalInstance, $log, $sc
 
   $scope.isPlaying = true;
 
-  $scope.isVideo = true;
+  $scope.isVideo = false;
   
   $scope.$on('changeTrack', function(event, args) {
       $scope.track = player.get();
@@ -279,7 +279,7 @@ radioApp.controller('FeatureCtrl', function ($scope, $http, $log, audio, player)
 
 /* Controller for menu on the homepage */
 radioApp.controller('MenuCtrl', function ($scope, $http, $log, audio, player) {
-  $http.get('/?json=get_category_posts&category_slug=tracks&count=200').
+  $http.get('/?json=get_category_posts&category_slug=tracks&count=9').
         then(function(response) {
             $scope.posts = response.data.posts;
         });
--- scripts/icon.js
+++ scripts/icon.js
@@ -1,5 +1,64 @@
 jQuery(function($){ 
 
+var $source = $(".container");
+var $mediatype = $source.data("mediatype");
+var $autoplay = $source.data("autoplay");
+var $metarefresh = $source.data("metarefresh");
+var $metatimeout = $source.data("metatimeout");
+var showartwork = $source.data("showartwork");
+var $src = $source.attr("src");
+var options = {};
+
+var isRunning = false;
+
+function getmetadata() {
+
+        if (!isRunning) {
+            console.log('getmetadata executing')
+            isRunning = true;
+            $.ajax({
+                type: "GET",
+                format: "json",
+                url: "//tx.sharp-stream.com/http_live.php?i=kissnational.mp3",
+                data: {
+                    src: $src
+                },
+                success: function success(msg) {
+                    console.log('success');
+                    isRunning = false;
+                },
+                error: function error(jqXHR, textStatus, errorThrown) {
+                    if (textStatus === "timeout") {
+                        isRunning = false;
+                    }
+                    console.log('error');
+                },
+            });
+        }
+    }
+
+
+setInterval(function(){
+  console.log('begin')
+  $.ajax({
+    type: "POST",
+    format: "json",
+    url: "/wp-content/themes/radio-serpentine/metadata.php",
+    data: {
+      streamurl: 'http://tx.sharp-stream.com/http_live.php?i=rsl7.mp3&device=website'
+    },
+    success: function success(msg) {
+       console.log('success')
+       console.log(msg);
+    },
+    error: function error(jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+    },
+  });
+}, 3000);
+
+
+
 const path = document.querySelector('#wave');
 const animation = document.querySelector('#moveTheWave');
 const m = 0.512286623256592433;
